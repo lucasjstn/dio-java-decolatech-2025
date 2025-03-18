08d7de7adc705770021c6aaf27431ae9fb5efb62
Lucas Oliveira <lucas7justino@gmail.com>
2025-03-18 20:01:20 -0300

    first commit

---
+++ src/main/java/me/dio/DioJavaDecolatech2025Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioJavaDecolatech2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioJavaDecolatech2025Application.class, args);
+	}
+
+}
